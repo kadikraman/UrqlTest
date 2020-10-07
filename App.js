035661938d22d1d7dd1cbd0e0739ec8a5ac4b69d
Kadi Kraman <kadi.kraman@formidable.com>
2020-10-07 11:12:03 +0100

    Add splash screen for iOS

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
 import { createClient, Provider, defaultExchanges } from 'urql';
 import { devtoolsExchange } from '@urql/devtools';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import ArtDetailScreen from './screens/ArtDetailScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
@@ -14,8 +16,12 @@ const client = createClient({
 });
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider value={client}>
+      <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
