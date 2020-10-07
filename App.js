1dc8bd9d278f6405ee08141c39c61716d3029f38
Kadi Kraman <kadi.kraman@formidable.com>
2020-10-07 09:37:10 +0100

    Update readme instructions and remove unused code

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import AsyncStorage from '@react-native-community/async-storage';
-import SInfo from 'react-native-sensitive-info';
-import {
-  createClient,
-  Provider,
-  dedupExchange,
-  cacheExchange,
-  fetchExchange,
-} from 'urql';
+import { createClient, Provider, defaultExchanges } from 'urql';
 import { devtoolsExchange } from '@urql/devtools';
-import { pipe, mergeMap, fromPromise, map } from 'wonka';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
@@ -17,46 +8,9 @@ import ArtDetailScreen from './screens/ArtDetailScreen';
 
 const Stack = createStackNavigator();
 
-const authExchange = ({ forward }) => {
-  return ops$ => {
-    return pipe(
-      ops$,
-      map(async operation => {
-        // 1 Async Storage - warnings
-        // const token = await AsyncStorage.getItem('test');
-
-        // 2 Sensitive Storage - warnings
-        // const token = await SInfo.getItem('test', {});
-
-        // 3. Promise.resolve - does not error
-        // const token = await new Promise(res => setTimeout(res, 500));
-
-        return {
-          ...operation,
-          context: {
-            ...operation.context,
-            fetchOptions: {
-              ...operation.context.fetchOptions,
-              headers: { Authorization: '' },
-            },
-          },
-        };
-      }),
-      mergeMap(fromPromise),
-      forward,
-    );
-  };
-};
-
 const client = createClient({
   url: 'https://metaphysics-production.artsy.net/',
-  exchanges: [
-    devtoolsExchange,
-    dedupExchange,
-    cacheExchange,
-    authExchange,
-    fetchExchange,
-  ],
+  exchanges: [devtoolsExchange, ...defaultExchanges],
 });
 
 const App = () => {
--- README.md
+++ README.md
@@ -1,9 +1,37 @@
-# Example app with urql and React Native
+# React Native urql example app
+
+This is an example app for a React Native app with [urql](https://github.com/FormidableLabs/urql).
+
+It uses:
+
+- [urql](https://github.com/FormidableLabs/urql)
+- [urql devtools](https://github.com/FormidableLabs/urql-devtools)
+- [artsy](https://github.com/artsy/metaphysics) art api
+- [React Navigation](https://reactnavigation.org/)
+
+## Setup instructions
+
+This is a plain React Native app, meaning that in order to run it, you'll need to have XCode (for iOS) or Android Studio (for Android) set up.
+
+First, clone the repo and install the JavaSctipt dependencies with
+
+```sh
+yarn
+```
+
+### iOS
 
 To run the app on iOS:
 
 ```sh
-yarn
-(cd ios && pod install)
+npx pod-install
 react-native run-ios
+```
+
+### Android
+
+To run the app on Android, you may need to open your Android emulator first. Then run:
+
+```sh
+react-native run-android
 ```
